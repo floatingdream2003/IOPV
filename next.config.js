3aa0a9124c50598b8c75a04ddb8d505502ba7a02
floatingdream2003 <912743486@qq.com>
2026-02-05 12:15:21 +0800

    1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',        // ? 开启静态导出
+  images: {
+    unoptimized: true,     // ? GitHub Pages 不支持 next/image 优化
+  },
   reactStrictMode: true,
 };
 
