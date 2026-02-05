a8751091f49d10f58756453d4be0b0e0f1aae0e2
floatingdream2003 <912743486@qq.com>
2026-02-05 12:07:24 +0800

    1

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  // 如果你的仓库名不是用户名.github.io，需要设置basePath
-  // basePath: '/your-repo-name',
-  // assetPrefix: '/your-repo-name/',
 };
 
 module.exports = nextConfig;
